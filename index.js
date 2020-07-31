100e999bfd012a4eec0d61e4eca2581e53911f02
Benjamin Jeffers <45741895+benjeffers@users.noreply.github.com>
2020-07-31 12:22:21 -0500

    Draw circles

--- index.js
+++ index.js
@@ -13,13 +13,28 @@ function getPosition() {
     };
 }
 
+function distance(circle1, circle2) {
+    d = Math.sqrt(Math.pow(circle1.x - circle2.x, 2) + Math.pow(circle1.y - circle2.y, 2))
+    return d
+}
+
+var circles = []
 
 function drawCircle(el) {
     var pos = getPosition()
-    ctx.beginPath();
-    console.log(pos.x)
-    ctx.arc(pos.x, pos.y, 50, 0, 2 * Math.PI);
-    ctx.stroke();
+    var canDraw = true;
+    for (let i = 0; i < circles.length; i++) {
+        if (!(distance(circles[i], pos) > 60)) canDraw = false;
+    }
+
+    if (canDraw) {
+        ctx.beginPath();
+        console.log(pos.x)
+        ctx.arc(pos.x, pos.y, 30, 0, 2 * Math.PI);
+        ctx.stroke();
+        circles.push(pos)
+    }
+
 }
 
 document.addEventListener("click", drawCircle);
